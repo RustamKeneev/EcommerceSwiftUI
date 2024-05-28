947a6b05342de58f7fa233e28bdd9523d6fb6561
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-05-28 11:15:13 +0600

    Initial Commit

---
+++ EcommerceSwiftUI/EcommerceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceSwiftUIApp.swift
+//  EcommerceSwiftUI
+//
+//  Created by Rustam Keneev on 28/5/24.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcommerceSwiftUIUITests/EcommerceSwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EcommerceSwiftUIUITestsLaunchTests.swift
+//  EcommerceSwiftUIUITests
+//
+//  Created by Rustam Keneev on 28/5/24.
+//
+
+import XCTest
+
+final class EcommerceSwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
